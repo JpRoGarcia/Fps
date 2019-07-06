b13694b86f6009b612f5d416e8052ae6691134bb
Juan Pablo Rojas García <juan.1020405@gmail.com>
2019-07-06 10:23:37 -0500

    Create Project

---
+++ MyFps/Assets/Scripts/FpsAim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsAim : MonoBehaviour
+{
+    //Se crean dos variables de tipo flotante
+    float mouseX;
+    float mouseY;
+
+    public bool InvertedMouse;// Se crea una variable de tipo booleano.
+
+    void Update()
+    {
+       
+        mouseX += Input.GetAxis("Mouse X");// Se accede al input para ver la posicion de X.  
+
+        if (InvertedMouse)//Se creo para poder invertir la camara del eje Y.
+        {
+            mouseY += Input.GetAxis("Mouse Y");// Se accede al input para ver la posicion de Y.  
+        } else
+        {
+            mouseY -= Input.GetAxis("Mouse Y");// Se accede al input para ver la posicion de Y.  
+        }
+
+        transform.eulerAngles = new Vector3(mouseY, mouseX, 0);//Se accede al eulerAngles y se le asigna un vector donde cambia X y Y.
+
+    }
+}
---
+++ MyFps/Assets/Scripts/FpsMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsMove : MonoBehaviour
+{
+
+    public GameObject a ;// Se crea un objeto publico;
+
+    public float speed = 0.1f;//Se crea una variable de tipo flotante;
+
+    void Update()
+    {   
+       if (Input.GetKey(KeyCode.W))//Se accede al input para detectar el ingreso del teclado
+        {
+            transform.position +=  transform.forward * speed;//Se accede a la posicion del objeto y se le dice que avance hacia adelante y esta se multiplica por la velocidad.      
+        }
+
+        if (Input.GetKey(KeyCode.S))//Se accede al input para detectar el ingreso del teclado
+        {
+            transform.position -= transform.forward * speed; //Se accede a la posicion del objeto y se le dice que avance hacia atras y esta se multiplica por la velocidad.
+        }
+
+        if (Input.GetKey(KeyCode.A))//Se accede al input para detectar el ingreso del teclado
+        {
+            transform.position -= transform.right * speed; //Se accede a la posicion del objeto y se le dice que avance hacia la izquierda y esta se multiplica por la velocidad.
+        }
+
+        if (Input.GetKey(KeyCode.D))//Se accede al input para detectar el ingreso del teclado
+        {
+            transform.position += transform.right * speed; //Se accede a la posicion del objeto y se le dice que avance hacia la Derecha y esta se multiplica por la velocidad.
+        }
+
+        float Rotar = a.transform.eulerAngles.y;//Se crea un flotante y se le agrega la posicion del eje Y del objeto;
+        transform.rotation = Quaternion.Euler(0.0f, Rotar, 0.0f);//Se accede a la rotacion y se le cambia la rotacion del eje Y;
+
+    }
+}
